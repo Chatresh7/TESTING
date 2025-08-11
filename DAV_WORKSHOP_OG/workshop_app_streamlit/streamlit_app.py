219e3cfb3648ab77be2b3309b249a5c770aae949
Chatresh Konchada <hemishkonchada@gmail.com>
2025-08-11 12:06:06 +0530

    Update streamlit_app.py

--- DAV_WORKSHOP_OG/workshop_app_streamlit/streamlit_app.py
+++ DAV_WORKSHOP_OG/workshop_app_streamlit/streamlit_app.py
@@ -16,29 +16,29 @@ import uuid # This was already in your code, but moved it to top for best practi
 #import streamlit as st
 
 # Function to set background
-'''def set_background(image_file):
-    with open(image_file, "rb") as image:
-        encoded = base64.b64encode(image.read()).decode()
-    st.markdown(
-        f"""
-        <style>
-        .stApp {{
-            background-image: url("data:image/jpg;base64,{encoded}");
-            background-size: cover;
-            background-repeat: no-repeat;
-            background-position: center;
-        }}
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
+#def set_background(image_file):
+ #   with open(image_file, "rb") as image:
+  #      encoded = base64.b64encode(image.read()).decode()
+   # st.markdown(
+    #    f"""
+     #   <style>
+      #  .stApp {{
+       #     background-image: url("data:image/jpg;base64,{encoded}");
+        #    background-size: cover;
+         #   background-repeat: no-repeat;
+          #  background-position: center;
+        #}}
+        #</style>
+        #""",
+        #unsafe_allow_html=True
+    #)
     
 
 # Call function here
-set_background("DAV_WORKSHOP_OG/workshop_app_streamlit/bj.jpg")
+#set_background("DAV_WORKSHOP_OG/workshop_app_streamlit/bj.jpg")
 
 # Rest of your Streamlit code
-#st.title("My App")'''
+#st.title("My App")
 
 
 def send_email(to_address, subject, message_body):
@@ -776,6 +776,7 @@ elif choice == "Logout":
     st.session_state.pop("last_team_user", None)
     st.success("✅ Logged out successfully! Redirecting to home...")
     safe_rerun()
+
 
 
 
